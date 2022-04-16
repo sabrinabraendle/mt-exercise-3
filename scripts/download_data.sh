e4a0099735d79f465d46dd8ba7af0b38ede43ce7
Sabrina Brändle <sabrina.braendle@uzh.ch>
2022-04-16 20:55:35 +0200

    add data and models

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,11 +5,11 @@ base=$scripts/..
 
 data=$base/data
 
-mkdir -p $data
+# mkdir -p $data
 
 tools=$base/tools
 
-# link default training data for easier access
+# TODO: link default training data for easier access
 
 mkdir -p $data/wikitext-2
 
@@ -18,26 +18,31 @@ for corpus in train valid test; do
     ln -snf $absolute_path $data/wikitext-2/$corpus.txt
 done
 
-# download a different interesting data set!
+# TODO: download a different interesting data set!
 
-mkdir -p $data/grimm
+mkdir -p $data/pride
 
-mkdir -p $data/grimm/raw
+mkdir -p $data/pride/raw
 
-wget https://www.gutenberg.org/files/52521/52521-0.txt
-mv 52521-0.txt $data/grimm/raw/tales.txt
+# wget https://www.gutenberg.org/files/52521/52521-0.txt
+wget https://www.gutenberg.org/files/1342/1342-0.txt
+mv 1342-0.txt $data/pride/raw/text.txt
 
-# preprocess slightly
+# TODO: preprocess slightly
 
-cat $data/grimm/raw/tales.txt | python $base/scripts/preprocess_raw.py > $data/grimm/raw/tales.cleaned.txt
+cat $data/pride/raw/text.txt | python $base/scripts/preprocess_raw.py > $data/pride/raw/text.cleaned.txt
 
-# tokenize, fix vocabulary upper bound
+# TODO: tokenize, fix vocabulary upper bound
 
-cat $data/grimm/raw/tales.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
-    $data/grimm/raw/tales.preprocessed.txt
+cat $data/pride/raw/text.cleaned.txt | python $base/scripts/preprocess.py --vocab-size 5000 --tokenize --lang "en" --sent-tokenize > \
+    $data/pride/raw/text.preprocessed.txt
 
-# split into train, valid and test
+# TODO: split into train, valid and test
 
-head -n 440 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/valid.txt
-head -n 840 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/test.txt
-tail -n 3075 $data/grimm/raw/tales.preprocessed.txt | head -n 2955 > $data/grimm/train.txt
+# head -n 440 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/valid.txt
+# head -n 840 $data/grimm/raw/tales.preprocessed.txt | tail -n 400 > $data/grimm/test.txt
+# tail -n 3075 $data/grimm/raw/tales.preprocessed.txt | head -n 2955 > $data/grimm/train.txt
+
+head -n 750 $data/pride/raw/text.preprocessed.txt | tail -n 700 > $data/pride/valid.txt
+head -n 1400 $data/pride/raw/text.preprocessed.txt | tail -n 700 > $data/pride/test.txt
+tail -n 7000 $data/pride/raw/text.preprocessed.txt | head -n 6000 > $data/pride/train.txt
--- scripts/train.sh
+++ scripts/train.sh
@@ -7,7 +7,7 @@ models=$base/models
 data=$base/data
 tools=$base/tools
 
-mkdir -p $models
+# mkdir -p $models
 
 num_threads=4
 device=""
@@ -15,11 +15,11 @@ device=""
 SECONDS=0
 
 (cd $tools/pytorch-examples/word_language_model &&
-    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/grimm \
+    CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/pride \
         --epochs 40 \
         --log-interval 100 \
-        --emsize 200 --nhid 200 --dropout 0.5 --tied \
-        --save $models/model.pt
+        --emsize 200 --nhid 200 --dropout 0.7 --tied \
+        --save $models/model_07.pt
 )
 
 echo "time taken:"
